19b6dfa1269639491c193ee890b9a7223001d709
Fabricio Araujo <fabricio_2310@hotmail.com>
2022-08-28 13:44:20 +0100

    Adicionado exemplo_1

---
+++ exemplo_1.py
@@ -0,0 +1,48 @@
+""" Tipos de Funções em Python """
+
+#1 Maneira tradicional de chmar uma função
+# Função Nomeada
+def funcao_nomeada():
+    return 'Oi'
+# Para chamar a função "minha_funcao" eu uso o abre e fecha parenteses
+# após seu nome -> () -> Call
+# >>> minha_funcao()
+# >>> 'Oi'
+
+#2 Função Anonima -> Lambda
+# Função lambda não precisa receber nome
+anonima = lambda : 'Oi'
+# Para chamar a função "anonima" eu uso o abre e fecha parenteses
+# após seu nome -> () -> Call
+# >>> anonima()
+# >>> 'Oi'
+
+#3 Outra manira de fazer uma função em python é usando Classes
+# Classe que se comporta como uma função
+class FuncaoClasse:
+    def __call__(self):
+        return 'Oi'
+#  >>> FuncaoClasse()
+#  <__main__.FuncaoClasse object at 0x100af73d0>
+#  >>> FuncaoClasse()()
+#  'oi'
+
+# Posso estanciar a FuncaoClasse() para uma outra variável:
+#  >>> minha_classe_funcao = FuncaoClasse()
+#  >>> minha_classe_funcao()
+#  'Oi'
+
+# São funções:
+#  >>> type(anonima)
+#  <class 'function'>
+
+#  >>> type(funcao_nomeada)
+#  <class 'function'>
+
+# Se comporta como uma função mais não é uma função de fato
+#  >>> type(FuncaoClasse)
+#  <class 'type'>
+#  >>> type(FuncaoClasse())
+#  <class '__main__.FuncaoClasse'>
+#  >>> type(FuncaoClasse().__call__)
+#  <class 'method'>
